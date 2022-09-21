8d384c0898d51961554ff2b2d62d0c6fc004352b
Dezeus <zeusawah@gmail.com>
2022-09-21 17:15:06 +0200

    all codes

--- Number-guessing/Program.cs
+++ Number-guessing/Program.cs
@@ -6,7 +6,31 @@ namespace Number_guessing
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random r = new Random();
+
+            int winNum = r.Next(0, 100);
+
+            bool win = false;
+
+            do
+            {
+                Console.WriteLine("Guess a number between 0 and 100: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                if (num > winNum)
+                    Console.WriteLine("Too high! Guess number smaller than {0}",num);
+                else if (num < winNum)
+                    Console.WriteLine("Too low! Guess a number higher than {0}",num);
+                else if (num == winNum)
+                {
+                    win = true;
+
+                    Console.WriteLine("You win!!!");
+                }
+            }
+            while (win == false) ;
+            Console.WriteLine("Enter any key to quit game ");
+            Console.ReadKey(true);
         }
     }
 }
